b1cc4b576e6aa7963c6ae45c167404a0671d392e
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-25 09:29:55 +0530

    add cofigs

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
 
+
+//config
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //connect mongodb
 mongoose.connect(
   "mongodb+srv://darshana:darshana@cluster0.uoooe.mongodb.net/school?retryWrites=true&w=majority"
