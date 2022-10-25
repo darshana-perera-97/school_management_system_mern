c4a12c0507130fc87badd8946b0de69a672f3bbe
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-25 09:28:47 +0530

    database link added

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
+
+//connect mongodb
+mongoose.connect(
+  "mongodb+srv://darshana:darshana@cluster0.uoooe.mongodb.net/school?retryWrites=true&w=majority"
+);
