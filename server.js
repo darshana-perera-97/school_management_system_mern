adc9cc4115477b8d41b5990b5a8a60f0911b09f1
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-27 22:44:04 +0530

    read routes created

--- server.js
+++ server.js
@@ -45,6 +45,55 @@ const Students = mongoose.model("Students", studentsSchema);
 const Teachers = mongoose.model("Teachers", techersSchema);
 const Courses = mongoose.model("Courses", coursesSchema);
 
+//read route
+app.get("/students", (req, res) => {
+  Students.find()
+    .then((admin) => res.json(admin))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+app.get("/teachers", (req, res) => {
+  Teachers.find()
+    .then((admin) => res.json(admin))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+app.get("/courses", (req, res) => {
+  Courses.find()
+    .then((admin) => res.json(admin))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+//filter - login
+app.get("/admin/:usr", (req, res) => {
+  const usr = req.params.usr;
+  Admin.find({ username: usr })
+    .then((admin) => res.json(admin))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+
+// create route
+app.post("/newuser", (req, res) => {
+  const newAdmin = new Admin({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  newAdmin
+    .save()
+    .then((item) => console.log(item))
+    .catch((err) => res.status(400).json("Error " + err));
+});
+app.post("/newst", (req, res) => {
+  const newStudent = new Students({
+    fname: req.body.fname,
+    lname: req.body.lname,
+  });
+
+  newStudent
+    .save()
+    .then((item) => console.log(item))
+    .catch((err) => res.status(400).json("Error " + err));
+});
 
 
 
---
+++ src/Design/Design.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginWindow from "./Login/LoginWindow";
+
+export default function Design() {
+  return (
+    <div>
+      <LoginWindow />
+    </div>
+  );
+}
