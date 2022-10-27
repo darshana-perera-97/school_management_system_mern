dea1d755926fd10973b045d701ddeabf4851be5b
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-27 22:31:27 +0530

    data schema created

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
 
-
 //config
 app.use(cors());
 app.use(express.json());
@@ -14,3 +13,33 @@ app.use(express.urlencoded({ extended: false }));
 mongoose.connect(
   "mongodb+srv://darshana:darshana@cluster0.uoooe.mongodb.net/school?retryWrites=true&w=majority"
 );
+
+//data schemas
+const admin = {
+  username: String,
+  password: String,
+};
+const students = {
+  fname: String,
+  lname: String,
+  age: String,
+  email: String,
+  gender: String,
+  grade: String,
+};
+const techers = {
+  fname: String,
+  lname: String,
+  courses: String,
+  experience: String,
+};
+const courses = {
+  courseName: String,
+  duration: String,
+  field: String,
+};
+
+//server
+app.listen(port, function () {
+  console.log("Express is running");
+});
