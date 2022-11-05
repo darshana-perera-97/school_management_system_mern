c70bd2edf6a4e3cf12fa4bfd33233b9ff24c3d2d
Darshana Perera <darshana.saluka.10@gmail.com>
2022-11-05 12:23:45 +0530

    add new student frontend created

---
+++ src/Design/Design.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+
+export default function Design() {
+  const [fname, setFname] = React.useState("");
+  const [lname, setLname] = React.useState("");
+  return (
+    <div>
+      <input
+        value={fname}
+        onChange={(e) => {
+          setFname(e.target.value);
+        }}
+      />
+      <input
+        value={lname}
+        onChange={(e) => {
+          setLname(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const newStudent = {
+            fname: fname,
+            fname: fname,
+          };
+        //   axios.post("/newst", newStudent);
+          // console.log(newItem);
+          // alert("item added");
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+}
