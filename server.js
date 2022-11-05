dd07786040c154039fe086e89f15bd2e73dff60b
Darshana Perera <darshana.saluka.10@gmail.com>
2022-11-05 12:07:49 +0530

    configurations added

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 //for backend
+
+//importing packages
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
+
+//config
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, function () {
   console.log("Express is running");
