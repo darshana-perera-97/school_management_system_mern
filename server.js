d720e20e1b7f787b7764c024f9907e252565d95b
Darshana Perera <darshana.saluka.10@gmail.com>
2022-11-05 12:03:40 +0530

    start the server

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
+
+
+app.listen(port, function () {
+  console.log("Express is running");
+});
+
